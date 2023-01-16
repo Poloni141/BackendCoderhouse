11dd8a8a5277d577850afbf7b59e0b948e539527
Nicolás Lázaro <121945143+NicolasLazaro@users.noreply.github.com>
2023-01-15 22:16:56 -0300

    Check if code exists

--- Ejercicio-clase3.js
+++ Ejercicio-clase3.js
@@ -13,14 +13,20 @@ class ProductManager {
             stock,
             id
         }
+
         if (this.products.length === 0) {
             product.id = 1
         } else {
             product.id = this.products[this.products.length - 1].id + 1
         }
 
-        this.products.push(product)
-        console.log(this.products)
+        let found = this.products.some(p => p.code === code)
+        if (!found) {
+            this.products.push(product)
+            console.log(this.products)
+        } else {
+            console.log('El codigo ya existe, intente con otro')
+        }
     }
 
     getProducts = () => this.products
@@ -37,5 +43,3 @@ class ProductManager {
 
 const p1 = new ProductManager()
 p1.addProduct('Yamaha R6', 'Motorbike', 20000, 'https://www.mundomotero.com/wp-content/uploads/2021/05/Yamaha-R6-RACE-2022-8-1200x675-1-1024x576.jpg', 1, 5, 1)
-
-console.log(p1.getProductById(1))
