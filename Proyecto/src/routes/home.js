b6b10339c8678c7efc2ab36d5d0781243493441d
Nicolás Lázaro <121945143+NicolasLazaro@users.noreply.github.com>
2023-02-16 12:33:44 -0300

    Testing

---
+++ Proyecto/src/routes/home.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import ProductManager from '../daos/product-manager.js'
+
+
+const motos = new ProductManager('./src/mockDB/productos.json')
+motos.getProducts()
+
+const router = Router()
+
+router.get('/home', async (request, response) => {
+    let products = await motos.getProducts()
+    return response.status(200).send(products)
+})
+
+export default router
--- Proyecto/src/server.js
+++ Proyecto/src/server.js
@@ -1,13 +1,20 @@
 import express from 'express'
+import  handlebars  from 'express-handlebars'
 
 import productRouter from './routes/products.js'
 import cartRouter from './routes/cart.js'
+
+
 
 
 const PORT = 8080
 const server = express()
 server.use(express.json())
 server.use(express.urlencoded({extended:true}))
+
+server.engine('handlebars', handlebars.engine())
+server.set('views', __dirname+'/views')
+server.set('views engine', 'handlebars')
 
 server.use('/api/products', productRouter)
 
